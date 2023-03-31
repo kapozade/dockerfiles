8737842551574850c5e65e5c8b6932dfcee3c7e0
Onur Kapçık <onurkapcik@gmail.com>
2023-03-31 21:27:41 +0300

    Add MongoDB compose yml and scripts. (#2)

---
+++ mongodb/README.md
@@ -0,0 +1,5 @@
+To create MongoDB replicaset on your local environment, run below statement on your commandline.
+
+```
+$ > bash ./create-cluster.sh
+```
---
+++ mongodb/initialize-replicaset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+mongo <<EOF
+var config = 
+{ 
+    "_id": "dbrs", 
+    "version": 1, 
+    "members": [ 
+        { 
+            "_id": 1, 
+            "host": "mongo1:27017", 
+            "priority": 3 
+        }, 
+        { 
+            "_id": 2, 
+            "host": "mongo2:27017", 
+            "priority": 2 
+        }, 
+        { 
+            "_id": 3, 
+            "host": "mongo3:27017", 
+            "priority": 1 
+        } 
+    ]
+};
+rs.initiate(config, { force: true });
+rs.status();
+
+sleep 30
+
+mongo <<EOF 
+    use admin; 
+    db.createUser({ user: "mongoadmin" , pwd: "mongoadmin", roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"]}); 
+    rs.status();
+EOF
