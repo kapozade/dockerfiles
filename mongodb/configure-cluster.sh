1a571bb9b666796220386c9a08465985bb4abaa8
Onur <onurkapcik@gmail.com>
2023-04-12 16:48:03 +0300

    Change mongodb restart policy.

--- mongodb/configure-cluster.sh
+++ mongodb/configure-cluster.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-mongo <<EOF
+echo configure-cluster.sh will be running in 15 seconds.
+sleep 15
+
+mongosh "mongo1:27017" <<EOF
 var config = 
 { 
     "_id": "dbrs", 
@@ -9,27 +12,24 @@ var config =
         { 
             "_id": 1, 
             "host": "mongo1:27017", 
-            "priority": 3 
+            "priority": 2
         }, 
         { 
             "_id": 2, 
             "host": "mongo2:27017", 
-            "priority": 2 
+            "priority": 0 
         }, 
         { 
             "_id": 3, 
             "host": "mongo3:27017", 
-            "priority": 1 
+            "priority": 0 
         } 
     ]
 };
 rs.initiate(config, { force: true });
 rs.status();
 
-sleep 30
-
-mongo <<EOF 
-    use admin; 
-    db.createUser({ user: "mongoadmin" , pwd: "mongoadmin", roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"]}); 
-    rs.status();
+use admin;
+db.createUser({ user: "dbadmin" , pwd: "dbadmin", roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"]}); 
+rs.status();
 EOF
--- mongodb/create-cluster.sh
+++ mongodb/create-cluster.sh
@@ -1,5 +1,1 @@
 docker-compose -f mongors.yml up -d
-
-sleep 20
-
-docker exec mongo1 /scripts/initialize-replicaset.sh
